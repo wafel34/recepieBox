a32692f897f68a7ddd7169d0a4627c9c9d0d477c
Pawel Maricnkowski <wafel34@gmail.com>
2017-08-12 14:59:14 +0100

    Added new componenet for each recepie

--- components/scripts/app.js
+++ components/scripts/app.js
@@ -2,32 +2,29 @@
 var $ = window.jQuery = require("jQuery"),
     React = require("react"),
     ReactDOM = require("react-dom"),
-    bootstrap = require("bootstrap-sass");
+    bootstrap = require("bootstrap-sass"),
+    SingleRecepie = require("./singleRecepie");
 
 class ReciepiesContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            data : [
+                {
+                    recepieName: "spaghetti",
+                    ingridients: ["pasta","meat","sauce"]
+                },
+                {
+                    recepieName: "ravioli cake",
+                    ingridients: ["ravioli","meat","sauce", "cheese"]
+                }
+            ]
+        };
+    }
     render () {
         return (
             <div clasName="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                <div className="panel panel-primary">
-                    <div className="panel-heading" role="tab" id="heading1">
-                        <div className="panel-title">
-                            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="true" aria-controls="collapseOne">
-                                Collapse 1
-                            </a>
-                        </div>
-                    </div>
-                    <div id="collapse1" className="panel-collapse collapse" role="tabpanel" aria-labelledby="heading1">
-                        <div className="panel-body">
-                            <ul className="list-group">
-                                <li className="list-group-item">List item 1</li>
-                                <li className="list-group-item">List item 2</li>
-                                <li className="list-group-item">List item 3</li>
-                            </ul>
-                            <button className="btn btn-primary">Edit</button>
-                            <button className="btn btn-danger">Delete</button>
-                        </div>
-                    </div>
-                </div>
+                <SingleRecepie />
             </div>
         );
     }
